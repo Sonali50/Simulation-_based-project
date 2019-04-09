77a89e3aa14f61a378158ac0dd9ea769ec26e49d
Sonali50 <49447556+Sonali50@users.noreply.github.com>
2019-04-09 10:36:02 -0700

    Update Qus_1.c

--- Qus_1.c
+++ Qus_1.c
@@ -16,6 +16,50 @@ int Queue(int t1)
 		return 2; 
 	}
 } 
+void cal()
+{
+
+		int arrival_time[10], burst_time[10], temp[10];
+	int i, smallest, count = 0, time, limit;
+	double wait_time = 0, turnaround_time = 0, end;
+	float average_waiting_time, average_turnaround_time;
+	burst_time[9] = 9999;  
+ 	for(time = 0; count != limit; time++)
+ 	{
+   		smallest = 9;
+  		for(i = 0; i < limit; i++)
+  		{
+   			if(arrival_time[i] <= time && burst_time[i] < burst_time[smallest] && burst_time[i] > 0)
+			{
+   				smallest = i;
+			}
+  		}
+  		burst_time[smallest]--;
+  		if(burst_time[smallest] == 0)
+  		{
+   			count++;
+   			end = time + 1;
+   			wait_time = wait_time + end - arrival_time[smallest] - temp[smallest];
+   			turnaround_time = turnaround_time + end - arrival_time[smallest];
+  		}
+ 	}
+ 	
+}
+void display()
+{
+float average_waiting_time, average_turnaround_time;
+	/*int limit;
+		double wait_time = 0, turnaround_time = 0, end;
+average_waiting_time = wait_time / limit; 
+	average_turnaround_time = turnaround_time / limit;*/
+ 	printf("\n\nAverage Waiting Time:\t%lf\n", average_waiting_time);
+    	printf("Average Turnaround Time:\t%lf\n", average_turnaround_time);	
+}
+
+
+
+
+
 int main() 
 { 
 	int no_p, count, temp_process, time, j, y; 
@@ -87,6 +131,8 @@ int main()
 		} 
 	} 
 	} 
+	cal();
+	display();
 	return 0; 
 }
 
